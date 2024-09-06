3b19594954111eaaadb12432a3eab64857285fe6
NameawaShinderu <73576564+NameawaShinderu@users.noreply.github.com>
2024-09-07 02:10:42 +0530

    fristcommit

---
+++ workspace/counter.js
@@ -0,0 +1,6 @@
+
+
+for (let i = 30; i>0 ; i--){
+    console.log(i)
+
+}
---
+++ workspace/main.js
@@ -0,0 +1,11 @@
+const personArray = ["harkirat", " raman " ," priYa"] ;
+const genderArray = ["male", "male","female"];
+
+
+
+for (let i = 0; i<genderArray.length; i++) {
+    if (genderArray[i] == "male"){
+        console. log ( personArray [i] ) ;
+    }
+}
+
